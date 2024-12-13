6843f5bb6ee66605d3ae5ae3b94130d2c5a316ab
Bernardo Nogueira <62897976+Samuraiflamesf@users.noreply.github.com>
2024-12-13 12:22:11 -0300

    controller pdf

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PdfController;
 
 Route::redirect('/', '/admin/login');
+Route::get('pdf/{stabilityConsultation}', PdfController::class)->name('pdf');
