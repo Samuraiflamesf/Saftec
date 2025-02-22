936051c391d4accf33cd7d3dd4e438b71cfc6c98
Bernardo Nogueira <samuraiflamesf@gmail.com>
2025-02-21 21:13:28 -0300

    install livewire

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Livewire\CardsPage;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PdfController;
 
-Route::redirect('/', '/admin/login');
+Route::redirect('/login', '/admin/login');
+Route::redirect('/', '/cards');
+
+/* Route::get('cards', CardsPage::class); */
+
 Route::get('pdf/{stabilityConsultation}', PdfController::class)->name('pdf');
