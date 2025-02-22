1f0d432e78966173280cc3346de7e08b9ab8975e
Bernardo Nogueira <samuraiflamesf@gmail.com>
2025-02-21 22:13:46 -0300

    New resource Card

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Livewire\CardsPage;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PdfController;
 
-Route::redirect('/', '/admin/login');
+Route::redirect('/login', '/admin/login');
+
+Route::get('cards', CardsPage::class);
+Route::redirect('/', '/cards');
+
 Route::get('pdf/{stabilityConsultation}', PdfController::class)->name('pdf');
